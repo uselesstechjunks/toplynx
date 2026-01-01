5b85aa1c45b8879da7a8ae318a615cfe9c498f15
uselesstechjunks <118470225+uselesstechjunks@users.noreply.github.com>
2026-01-01 22:49:40 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,8 +41,9 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'python_docs_theme'
+# html_theme = 'sphinx_rtd_theme'
+# html_theme = 'python_docs_theme'
+html_theme = 'sphinx-wagtail-theme'
 html_sidebars = {
 	'**': ['globaltoc.html', 'sourcelink.html', 'searchbox.html'], 
 	'using/windows': ['windows-sidebar.html', 'searchbox.html'],
